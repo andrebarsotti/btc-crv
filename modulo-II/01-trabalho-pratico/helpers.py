6c6151596d5ee0a8a2f9bc12131c0fea70df1997
Andre Salvadeo <andresalvadeo@hotmail.com>
2023-04-03 20:48:27 -0300

    [MOD-II-TP]-Salvando dados da cotação histórica no sqllite

--- modulo-II/01-trabalho-pratico/helpers.py
+++ modulo-II/01-trabalho-pratico/helpers.py
@@ -1,4 +1,6 @@
 import os
+import pandas as pd
+
 from dotenv import load_dotenv
 
 
@@ -6,3 +8,7 @@ load_dotenv()
 
 def get_env_var(var_name):
     return os.getenv(var_name)
+
+def verificar_dados_faltando(df):
+    contagem_faltantes = df.isnull().sum()
+    return contagem_faltantes[contagem_faltantes > 0].sort_values(ascending=False)
