866bd949235a64a18e087b627db8a7a3515f194f
André Claudinei Barsotti <andrebarsotti@gmail.com>
2023-04-10 18:41:09 -0300

    [MOD-II-DSF]-Ajuste no caminho dos dados brutos das empresas

--- modulo-II/02-desafio/src/config.py
+++ modulo-II/02-desafio/src/config.py
@@ -1,3 +1,3 @@
-ARQUIVO_DADOS_EMPRESA_TEMPLATE = "../00-dados-brutos/dados_empresas_cvm_$letra.html"
+ARQUIVO_DADOS_EMPRESA_TEMPLATE = "../00-dados-brutos/cadastro_empresas_cvm/dados_empresas_cvm_$letra.html"
 
 ARQUIVO_DADOS_EMPRESA_CVM = "../01-dados-limpos/empresas-cvm.csv"
